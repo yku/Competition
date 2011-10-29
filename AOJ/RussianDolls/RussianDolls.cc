084654d603b9d5e5c27f863fd80c91770be26aaa
yku <kz2yz4@gmail.com>
2011-10-29 16:29:59 +0900

    solved AOJ0157

---
+++ AOJ/RussianDolls/RussianDolls.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+    if(a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+/**
+ * dp[i] := 人形a[i]が含むことのできる最大の人形の数
+ * 1. dp[i] := 1 if どの人形も含むことができないとき(自分自身のみ）
+ * 2. dp[i] := dp[j]+1 if a[i] > a[j].ただし、aは昇順にソート済み
+ */
+int solve(vector<pair<int, int> > &dolls)
+{
+    int n = dolls.size();
+    int dp[n];
+    int ret = 0;
+    int wrong = 0;
+
+    fill(dp, dp+n, 1);
+    for(int i = 0; i < n; i++) {
+        int h1 = dolls[i].first;
+        int r1 = dolls[i].second;
+        
+        for(int j = 0; j < n; j++) {
+            int h2 = dolls[j].first;
+            int r2 = dolls[j].second;
+
+            if(h1 > h2 and r1 > r2) {
+                dp[i] = max(dp[i], dp[j] + 1);
+                //ret = max(ret, dp[i]); <-これは通らなかった.一つも含めることができない場合、retが更新されず0を返すから. もし通したいならret=1で初期化すればok
+                //wrong = max(wrong, dp[i]);
+            }
+        }
+        ret = max(ret, dp[i]);
+    }
+   
+    //cout << "[wrong]=" << wrong << " [ret]=" << ret << endl;
+
+    return ret;
+}
+
+int main()
+{
+    while(true) {
+        int n, m;
+        vector<pair<int, int> > dolls;
+
+        cin >> n;
+        if(n == 0) break;
+        for(int i = 0; i < n; i++) {
+            int h, r;
+            cin >> h >> r;
+            dolls.push_back(pair<int,int>(h, r));
+        }
+        cin >> m;
+        for(int i = 0; i < m; i++) {
+            int h, r;
+            cin >> h >> r;
+            dolls.push_back(pair<int,int>(h, r));
+        }
+        sort(dolls.begin(), dolls.end(), cmp);
+        cout << solve(dolls) << endl;
+    }
+    return 0;
+}
