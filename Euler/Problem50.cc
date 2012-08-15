94b92ab81f611fefc218142b74e3413a60767e8f
yku <kz2yz4@gmail.com>
2012-08-15 22:42:44 +0900

    fix algorithm

--- Euler/Problem50.cc
+++ Euler/Problem50.cc
@@ -9,11 +9,12 @@ using namespace std;
 typedef long long ll;
 
 #define MAX_N 1000001
+#define IS_PRIME(X) d[X]
 vector<int> primes;
+bool d[MAX_N];
 
 void init(void)
 {
-    bool d[MAX_N];
     fill(d, d+sizeof(d), true);
     d[0] = d[1] = false;
     for(int i = 2; i*i < MAX_N; i++) {
@@ -26,7 +27,10 @@ void init(void)
         if(d[i]) primes.push_back(i);
 
 }
-
+/**
+ * 素数nを与えて、nに一致するか超えるかまでを小さい素数の和で求めていく
+ * => 答えはでるが遅すぎる.
+ */
 int solve(int n)
 {
     int t = 0, c = 0, b = 0;
@@ -48,12 +52,19 @@ int main()
     
     int ans = 0;
     int max_t = 0;
-    
+   
+    // 小さい素数から足していき、できた数字が素数かどうかで判定する
     for(int i = 0; i < primes.size(); i++) {
-        int t = solve(primes[i]);
-        if(max_t < t) {
-            max_t = t;
-            ans = primes[i];
+        int t = 1;
+        int n = primes[i];
+        for(int j = i+1; j < primes.size(); j++) {
+            n += primes[j];
+            t++;
+            if(n >= MAX_N) break;
+            if(IS_PRIME(n) and max_t < t){
+                max_t = t;
+                ans = n;
+            }
         }
     }
     cout << ans << endl;
