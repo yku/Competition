892046f3e80fe3d30fa12ca77f5cbba2d0c764fa
yku <kz2yz4@gmail.com>
2014-04-13 14:28:26 +0900

    modify

--- gcj/2014/CookieClickerAlpha.cc
+++ gcj/2014/CookieClickerAlpha.cc
@@ -9,6 +9,32 @@
 using namespace std;
 typedef long long ll;
 
+/**
+ * 購入したほうがよいFarmの値+1を返す
+ */
+int getN(double C, double F, double X) {
+    double f = (X-C)/C;
+    f *= F;
+    double dn = (f-2)/F;
+    int n = (int)(dn + 0.5);
+    if(n < 0) n = 0;
+    
+    return n;
+}
+
+double getTime(double C, double F, double X, int n)
+{
+    double time = 0.0;
+    double rate = 2;
+    // ここで(n-1)番目のFarmまで買う.0-basedなのに注意
+    for(int i = 0; i < n; i++) {
+        time += (C/rate);
+        rate += F;
+    }
+    time += X/rate;
+    return time;
+}
+
 int main()
 {
     int T;
@@ -16,18 +42,10 @@ int main()
     cin >> T;
     for(int i = 1; i <= T; i++) {
         double C, F, X;
-        double ans = 1000000.0;
+        double ans = 0.0; 
         cin >> C >> F >> X;
-       
-        for(int n = 0; true; n++) {
-            double t = 0.0;
-            for(int k = 0; k < n; k++) {
-                t += C / (2+k*F);    
-            }
-            t += X / (2+n*F);
-            if(ans > t) ans = t;
-            else break;
-        }
+        int n = getN(C, F, X);  
+        ans = getTime(C, F, X, n);
         cout << "Case #" << i << ": " << fixed << setprecision(7) << ans << endl; 
     }
 
